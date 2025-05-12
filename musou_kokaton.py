a431a4ee3ba3c161b76e10f97ead89207bb0004b
c0a2406616 <c0a2406616@edu.teu.ac.jp>
2025-05-12 09:51:46 +0900

    追加機能１：高速化

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -10,7 +10,7 @@ WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
+  
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
     オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
